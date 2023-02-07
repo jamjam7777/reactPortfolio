308db6983423de384c9aa88f6d97cd855d8835cf
“Jamie <jamiedevine77@gmail.com>
2023-02-07 16:18:31 -0600

    latest

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <Col size={12} sm={6}>
+            <img src="" alt="Logo" />
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="#"><img src="" alt="Icon" /></a>
+              <a href="#"><img src="" alt="Icon" /></a>
+              <a href="#"><img src="" alt="Icon" /></a>
+            </div>
+            <p>Copyright 2023. All Rights Reserved</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
