8c67290afc6b417d2682bc3843176743b3e5020d
“Jamie <jamiedevine77@gmail.com>
2023-02-17 16:14:55 -0600

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { NavDropdown } from "react-bootstrap";
 
 
 
@@ -27,19 +28,19 @@ export const NavBar = () => {
     }
 
     return (
-        <Navbar expand="lg" className={scrolled ? 'scrolled': ""}>
+        <Navbar expand="md" className={scrolled ? 'scrolled': ""}>
       <Container>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
-            <span className="navbar-toggle-icon"></span>
+            <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-            <Nav.Link href="#Portfolio" className={activeLink === 'Portfolio' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('Portfolio')}>Work</Nav.Link>
+            <Nav.Link href="#work" className={activeLink === 'work' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('work')}>Work</Nav.Link>
             <Nav.Link href="#about" className={activeLink === 'about' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('about')}>About</Nav.Link>
           </Nav>
           <Nav.Link>
-          <a className="email-link" onClick={() => window.location = 'mailto:jamjamportfolio@gmail.com'}>
+          <a className="email-link" onClick={() => window.location = 'mailto:jamjamportfolio@gmail.com'} style={{ textDecoration: 'none' }} >
 Say Hello</a></Nav.Link>
             
         </Navbar.Collapse>
