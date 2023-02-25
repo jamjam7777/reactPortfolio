91c8e2c92eda48ad397405becbff61ac13464ad3
“Jamie <jamiedevine77@gmail.com>
2023-02-24 22:01:25 -0600

    formatting

--- src/components/WorkCards.js
+++ src/components/WorkCards.js
@@ -15,8 +15,8 @@ export const WorkCards = ({ title, description, imgUrl, URL, intro }) => {
           </div>
         </Col>
         <Col xs={12} md={6} xl={5}>
-          <div className="work-p">
-            <h1>{intro}</h1>
+          <div className="project-p">
+            <div className="project-header"><h1>{intro}</h1></div>
             <p>{description}</p>
           </div>
         </Col>
