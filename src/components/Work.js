ccd119da2a4d78542bd03fcf3dbeb29401fefecf
“Jamie <jamiedevine77@gmail.com>
2023-03-14 16:12:26 -0500

    addedsites

--- src/components/Work.js
+++ src/components/Work.js
@@ -5,27 +5,31 @@ import "animate.css";
 export const Work = () => {
   const projects = [
     {
-      title: "Adventuretime Gallery",
       URL: "https://jamjam7777.github.io/jamjamadventures/",
       imgUrl:
         "https://storage.googleapis.com/bucket_la_gringa_loca/laGringaLoca/img/adventures.png",
-      intro: "React Travel Photo Project",
-      description: "Designed a react website that highlights travel photos. This site has a clean feel, is easy to navigate, and minimalistic in its aesthetics. Easy to move between photos, with a simple socials link in the footer.",
+      title: "Photo Gallery",
+      description: "Designed a React website that highlights travel photos. This site has a clean feel, is easy to navigate, and minimalistic in its aesthetics. Easy to move between photos, with a simple socials link in the footer.",
     },
     {
-      title: "La Gringa Art",
+      URL: "https://snack.expo.dev/@jamjam77/github.com-jamjam7777-mood-words?platform=ios",
+      imgUrl:
+        "https://storage.googleapis.com/bucket_la_gringa_loca/laGringaLoca/img/mood-words.png",
+      title: "Mood Words App",
+      description: "This React Native app was inspired by affirmations. We all need a good affirmation, but it not always meshes with our current mood. I created an app that gives you an affiramtion based on your mood. Easy to navigate, simple calming colors, and added a function to set reminders or have the affirmation as a widget on your phone",
+    },
+    {
       URL: "https://www.etsy.com/shop/TheGringaLoca?ref=seller-platform-mcnav",
       imgUrl:
         "https://storage.googleapis.com/bucket_la_gringa_loca/laGringaLoca/img/gringa.png",
-        intro: "La Gringa Art Project",
-      description: "Still under construction, this react native app will showcase the art of La Gringa, to further promote en Etsy store. This app will be easy to navigate, colorful, with a dash of whimsy. It will have interactive elements and a fun activity to add to this colorful art.",
+        title: "La Gringa Art",
+      description: "Still under construction, this React site will showcase the art of La Gringa, to further promote the Etsy store. The site will be easy to navigate, colorful, with a dash of whimsy. It will have interactive elements and a fun activity to add to this colorful art.",
     },
     {
-      title: "Html/bootstrap",
       URL: "https://jamjam7777.github.io/jamjamportfolio",
       imgUrl:
         "https://storage.googleapis.com/bucket_la_gringa_loca/laGringaLoca/img/portfolio.png",
-        intro: "The OG Portfolio Project",
+        title: "OG Portfolio",
       description: "Original portfolio design, exclusively in html/bootstrap. This project had the challenges of formatting from large to small screens, and a full jpeg background. Colorful yet simple, this website it easy to navigate",
     },
   ];
--- src/components/WorkCards.js
+++ src/components/WorkCards.js
@@ -1,19 +1,19 @@
 import { Col, Container, Row } from "react-bootstrap";
 
-export const WorkCards = ({ title, description, imgUrl, URL, intro }) => {
+export const WorkCards = ({ title, description, imgUrl, URL }) => {
   return (
     <Container>
       <Row>
-        <Col xs={12} md={6} xl={7}>
+        <Col xs={12} md={6} xl={6}>
           <div className="work-img">
             <a href={URL} target="_blank">
               <img src={imgUrl} />
             </a>
           </div>
         </Col>
-        <Col xs={12} md={6} xl={5}>
+        <Col xs={12} md={6} xl={6}>
           <div className="project-p">
-            <div className="project-header"><h1>{intro}</h1></div>
+            <div className="project-header"><h1>{title}</h1></div>
             <p>{description}</p>
           </div>
         </Col>
