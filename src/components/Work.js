fada95e4bb97f92ff54e9cb5194c6bb7858738fc
“Jamie <jamiedevine77@gmail.com>
2023-05-08 16:06:19 -0500

    fix img code

--- src/components/Work.js
+++ src/components/Work.js
@@ -10,28 +10,28 @@ export const Work = () => {
   const projects = [
     {
       URL: "https://jamjam7777.github.io/jamjamadventures/",
-      src: {adventures},
+      src: adventures,
       title: "Photo Gallery",
       description: "Designed a React website that highlights travel photos. This site has a clean feel, is easy to navigate, and minimalistic in its aesthetics. Easy to move between photos, with a simple socials link in the footer.",
     },
     {
       URL: "https://snack.expo.dev/@jamjam77/github.com-jamjam7777-mood-words?platform=ios",
       src:
-       {moodwords},
+       moodwords,
       title: "Mood Words App",
       description: "This React Native app was inspired by affirmations. We all need a good affirmation, but it not always meshes with our current mood. I created an app that gives you an affiramtion based on your mood. Easy to navigate, simple calming colors, and added a function to set reminders or have the affirmation as a widget on your phone",
     },
     {
       URL: "https://jamjam7777.github.io/LaGringa/",
       src:
-       {gringa},
+       gringa,
         title: "La Gringa Art",
       description: "Still under construction, this React site will showcase the art of La Gringa, to further promote the Etsy store. The site will be easy to navigate, colorful, with a dash of whimsy. It will have interactive elements and a fun activity to add to this colorful art.",
     },
     {
       URL: "https://jamjam7777.github.io/jamjamportfolio",
       src:
-        {portfolio},
+        portfolio,
         title: "OG Portfolio",
       description: "Original portfolio design, exclusively in html/bootstrap. This project had the challenges of formatting from large to small screens, and a full jpeg background. Colorful yet simple, this website it easy to navigate",
     },
